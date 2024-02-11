3bf6936af1c4b9a43cec3b2329c8e33fb713f297
jolian5532 <jolianmohmmad@gmail.com>
2024-02-11 13:14:23 +0100

    tiny refactor

--- main.py
+++ main.py
@@ -1,67 +1,258 @@
 import sys
-import struct
+import json
+
 
 class ElfFile:
     def __init__(self, file_path):
         self.readFromFile(file_path)
-        
-    def getBytes(self,start, nbytes,x=0):
-        if not x:
-            return self.lend(self.file_data[start:start+nbytes])
+
+    def getBytes(self, start, nbytes, x=0):
+        if x == 0:
+            return self.file_data[start : start + nbytes]
         else:
-            return self.file_data[start:start+nbytes]
-    
+            return x[start : start + nbytes]
+
     def readFromFile(self, file):
         f = open(file, "rb")
         self.file_data = f.read()
 
-    def lend(self,bytes):
+    def lend(self, bytes):
         return bytes[::-1]
 
     def deser(self):
-        self.e_ident = self.getBytes(0,16)      # File ident
-        self.EI_IMG = self.getBytes(0,4)        # ELF Signature
-        self.EI_CLASS = self.getBytes(4,2)      # 64Bit , Endian
-        self.EI_VERSION = self.getBytes(6,4)    # Always 1
-        self.e_type = self.getBytes(16, 2)      # Obj/File type
-        self.e_machine = self.getBytes(18, 2)   # ARCH
-        self.e_version = self.getBytes(20, 4)   # ELF version (1)
-        self.e_entry = self.getBytes(24, 8)     # Entry point
-        self.e_phoff = self.getBytes(32, 8)     # Program header offset
-        self.e_shoff = self.getBytes(40, 8)     # Section header offset
-        self.e_flags = self.getBytes(48, 4)     # ARCH flags
-        self.e_ehsize = self.getBytes(52, 2)    # Size of ELF header in Bytes
-        self.e_phentsize = self.getBytes(54, 2) # Size of program header entry
-        self.e_phnum = self.getBytes(56, 2)     # Number of program header entries
-        self.e_shentsize = self.getBytes(58, 2) # Size of section header entry
-        self.e_shnum = self.getBytes(60, 2)     # Number of section header entries
-        self.e_shstrndx = self.getBytes(62, 2)  # Section name strings section
-    def print_header(self):
-        print(f"e_ident: {self.e_ident.hex()}")
-        print(f"EI_IMG: {self.EI_IMG.hex()}")
-        print(f"EI_CLASS: {self.EI_CLASS.hex()}")
-        print(f"EI_VERSION: {self.EI_VERSION.hex()}")
-        print(f"e_type: {self.e_type.hex()}")
-        print(f"e_machine: {self.e_machine.hex()}")
-        print(f"e_version: {self.e_version.hex()}")
-        print(f"e_entry: {self.e_entry.hex()}")
-        print(f"e_phoff: {self.e_phoff.hex()}")
-        print(f"e_shoff: {self.e_shoff.hex()}")
-        print(f"e_flags: {self.e_flags.hex()}")
-        print(f"e_ehsize: {self.e_ehsize.hex()}")
-        print(f"e_phentsize: {self.e_phentsize.hex()}")
-        print(f"e_phnum: {self.e_phnum.hex()}")
-        print(f"e_shentsize: {self.e_shentsize.hex()}")
-        print(f"e_shnum: {self.e_shnum.hex()}")
-        print(f"e_shstrndx: {self.e_shstrndx.hex()}")
+        self.elffile64 = {
+            "e_ident": {
+                "offset": 0,
+                "size": 16,  # in bytes
+                "description": "ELF Identification",
+                "value": self.getBytes(0, 16),
+                "readValue": self.lend(self.getBytes(0, 16)).hex(),
+            },
+            "EI_IMG": {
+                "offset": 0,
+                "size": 4,
+                "description": "ELF Signature",
+                "value": self.getBytes(0, 4),
+                "readValue": self.lend(self.getBytes(0, 4)).hex(),
+            },
+            "EI_CLASS": {
+                "offset": 4,
+                "size": 2,
+                "description": "64Bit , Endian",
+                "value": self.getBytes(4, 2),
+                "readValue": self.lend(self.getBytes(4, 2)).hex(),
+            },
+            "EI_VERSION": {
+                "offset": 6,
+                "size": 4,
+                "description": "Always 1",
+                "value": self.getBytes(6, 4),
+                "readValue": self.lend(self.getBytes(6, 4)).hex(),
+            },
+            "e_type": {
+                "offset": 16,
+                "size": 2,
+                "description": "Obj/File type",
+                "value": self.getBytes(16, 2),
+                "readValue": self.lend(self.getBytes(16, 2)).hex(),
+            },
+            "e_machine": {
+                "offset": 18,
+                "size": 2,
+                "description": "ARCH",
+                "value": self.getBytes(18, 2),
+                "readValue": self.lend(self.getBytes(18, 2)).hex(),
+            },
+            "e_version": {
+                "offset": 20,
+                "size": 4,
+                "description": "ELF version (1)",
+                "value": self.getBytes(20, 4),
+                "readValue": self.lend(self.getBytes(20, 4)).hex(),
+            },
+            "e_entry": {
+                "offset": 24,
+                "size": 8,
+                "description": "Entry point",
+                "value": self.getBytes(24, 8),
+                "readValue": self.lend(self.getBytes(24, 8)).hex(),
+            },
+            "e_phoff": {
+                "offset": 32,
+                "size": 8,
+                "description": "Program header offset",
+                "value": self.getBytes(32, 8),
+                "readValue": self.lend(self.getBytes(32, 8)).hex(),
+            },
+            "e_shoff": {
+                "offset": 40,
+                "size": 8,
+                "description": "Section header offset",
+                "value": self.getBytes(40, 8),
+                "readValue": self.lend(self.getBytes(40, 8)).hex(),
+            },
+            "e_flags": {
+                "offset": 48,
+                "size": 4,
+                "description": "ARCH flags",
+                "value": self.getBytes(48, 4),
+                "readValue": self.lend(self.getBytes(48, 4)).hex(),
+            },
+            "e_ehsize": {
+                "offset": 52,
+                "size": 2,
+                "description": "Size of ELF header in Bytes",
+                "value": self.getBytes(52, 2),
+                "readValue": self.lend(self.getBytes(52, 2)).hex(),
+            },
+            "e_phentsize": {
+                "offset": 54,
+                "size": 2,
+                "description": "Size of program header entry",
+                "value": self.getBytes(54, 2),
+                "readValue": self.lend(self.getBytes(54, 2)).hex(),
+            },
+            "e_phnum": {
+                "offset": 56,
+                "size": 2,
+                "description": "Number of program header entries",
+                "value": self.getBytes(56, 2),
+                "readValue": self.lend(self.getBytes(56, 2)).hex(),
+            },
+            "e_shentsize": {
+                "offset": 58,
+                "size": 2,
+                "description": "Size of section header entry",
+                "value": self.getBytes(58, 2),
+                "readValue": self.lend(self.getBytes(58, 2)).hex(),
+            },
+            "e_shnum": {
+                "offset": 60,
+                "size": 2,
+                "description": "Number of section header entries",
+                "value": self.getBytes(60, 2),
+                "readValue": self.lend(self.getBytes(60, 2)).hex(),
+            },
+            "e_shstrndx": {
+                "offset": 62,
+                "size": 2,
+                "description": "Section name strings section",
+                "value": self.getBytes(62, 2),
+                "readValue": self.lend(self.getBytes(62, 2)).hex(),
+            },
+            "PHeaders": [],
+        }
 
-    def getPHeaders(self):
-        self.PHeaders = []
-        headersBegin = int.from_bytes(self.e_phoff, "big")
-        headersSize = int.from_bytes(self.e_phentsize, "big") * int.from_bytes(self.e_phnum, "big")
-        return [self.getBytes(headersBegin,headersSize,1)[i:i+int.from_bytes(self.e_phentsize, "big")] for i in range(0, len(self.getBytes(headersBegin,headersSize,1)), int.from_bytes(self.e_phentsize, "big"))]
-        
-    
-a  = ElfFile(str(sys.argv[1]))
+        # Construct Program Headers
+        PHeadersOffset = int(self.elffile64["e_phoff"]["readValue"], 16)
+        PHeaderSize = int(self.elffile64["e_phentsize"]["readValue"], 16)
+        PHeadersAmount = int(self.elffile64["e_phnum"]["readValue"], 16)
+        PHeaders = self.getBytes(PHeadersOffset, PHeaderSize * PHeadersAmount)
+        PHeaders = [
+            PHeaders[i : i + PHeaderSize] for i in range(0, len(PHeaders), PHeaderSize)
+        ]
+        for index, PHeader in enumerate(PHeaders):
+            header_entry = {
+                "index": {
+                    "value": index,
+                    "readValue": index,
+                },
+                "p-type": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 0,
+                    "size": 4,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 0, 4
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 0, 4)
+                    ).hex(),
+                    "description": "Segment Type",
+                },
+                "p-flags": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 4,
+                    "size": 4,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 4, 4
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 4, 4)
+                    ).hex(),
+                    "description": "Segment Flags",
+                },
+                "p-offset": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 8,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 8, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 8, 8)
+                    ).hex(),
+                    "description": "Segment Offset",
+                },
+                "p-vaddr": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 16,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 16, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 16, 8)
+                    ).hex(),
+                    "description": "Segment Virtual Address",
+                },
+                "p-paddr": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 24,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 24, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 24, 8)
+                    ).hex(),
+                    "description": "Segment Physical Address",
+                },
+                "p-filesz": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 32,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 32, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 32, 8)
+                    ).hex(),
+                    "description": "Segment File Size",
+                },
+                "p-memsz": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 40,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 40, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 40, 8)
+                    ).hex(),
+                    "description": "Segment Memory Size",
+                },
+                "p-align": {
+                    "offset": (index * PHeaderSize) + PHeadersOffset + 48,
+                    "size": 8,
+                    "value": self.getBytes(
+                        (index * PHeaderSize) + PHeadersOffset + 48, 8
+                    ),
+                    "readValue": self.lend(
+                        self.getBytes((index * PHeaderSize) + PHeadersOffset + 48, 8)
+                    ).hex(),
+                    "description": "Segment Alignment",
+                },
+            }
+            self.elffile64[f"PHeaders"].append(header_entry)
+
+
+a = ElfFile(str(sys.argv[1]))
+
 a.deser()
-print(a.getPHeaders()[0].hex())
+# for key, value in a.elffile64.items():
+#   print(value["description"] + " : " + str(value["readValue"]))
+
+print(a.elffile64["PHeaders"][2])
